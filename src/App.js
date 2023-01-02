287de64c05aa8804fb408665d7003700026ec2d4
Shrutika Shaw <66830887+VenusTokyo@users.noreply.github.com>
2023-01-02 13:54:06 +0530

    added chatroom functionalities

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
 
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/firestore'
-import 'firebase/compat/auth'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import 'firebase/analytics';
 
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { useCollectionData } from 'react-firebase-hooks/firestore'
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+// import { Firestore } from 'firebase/firestore';
 
 firebase.initializeApp({
   apiKey: "AIzaSyD1iWi6HMVYRUSlw7DcxfHNdsJodVCR9-w",
@@ -18,59 +19,117 @@ firebase.initializeApp({
   appId: "1:235211429078:web:2916305ad6646a07dda577",
   measurementId: "G-DZKQLSP73G"
 })
+
 const auth = firebase.auth();
 const firestore = firebase.firestore();
+// const analytics = firebase.analytics();
 
-const [user] = useAuthState(auth)
+
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header>
-
-
+        <h1>ChatRoom</h1>
+        <SignOut />
       </header>
+
       <section>
         {user ? <ChatRoom /> : <SignIn />}
       </section>
+
     </div>
   );
 }
 
 function SignIn() {
+
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider)
+    auth.signInWithPopup(provider);
   }
+
   return (
     <>
-      <button onClick={signInWithGoogle}>Sign in with Google</button>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p id='community-txt'>Do not violate the community guidelines or you will be banned for life!</p>
     </>
-
   )
+
 }
+
 function SignOut() {
   return auth.currentUser && (
-    <button onClick={() => auth.signOut()}>Sign Out</button>
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
   )
 }
+
 
 function ChatRoom() {
-  const messagesRef = firestore.collection('messages')
-  const query = messagesRef.orderBy('createdAt').limit(25)
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
 
-  const [messages] = useCollectionData(query, { idField: 'id' })
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+
+  const sendMessage = async (e) => {
+
+    e.preventDefault();
+
+    const {uid} = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid
+  
+      
+      
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
   return (
-    <>
-      <div>
-        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-      </div>
-    </>
-  )
+  <>
+    <main>
 
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>Send</button>
+
+    </form>
+  </>)
 }
+
+
 function ChatMessage(props) {
-  const {text,uid}=props.message;
-  return <p>{text}</p>
+  const { text, uid} = props.message;
 
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <img src= {`https://avatars.dicebear.com/api/pixel-art-neutral/${uid}.svg`}/>
+      <p>{text}</p>
+    </div>
+  </>)
 }
+
+
 export default App;
+
